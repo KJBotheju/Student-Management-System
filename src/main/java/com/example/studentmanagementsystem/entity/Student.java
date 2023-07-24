d1ccc9314e8f9693bdfb34abf6c3ea91141131af
Kavinda Botheju <117912732+KJBotheju@users.noreply.github.com>
2023-07-24 23:50:26 +0530

    Initial commit

---
+++ src/main/java/com/example/studentmanagementsystem/entity/Student.java
@@ -0,0 +1,8 @@
+package com.example.studentmanagementsystem.entity;
+
+public class Student {
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+}
