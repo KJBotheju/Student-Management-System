1100f0fef1af23840459d0c392e0f185a19d6cbd
Kavinda Botheju <117912732+KJBotheju@users.noreply.github.com>
2023-07-25 17:28:45 +0530

    add student

--- src/main/java/com/example/studentmanagementsystem/controller/StudentController.java
+++ src/main/java/com/example/studentmanagementsystem/controller/StudentController.java
@@ -1,9 +1,12 @@
 package com.example.studentmanagementsystem.controller;
 
+import com.example.studentmanagementsystem.entity.Student;
 import com.example.studentmanagementsystem.service.StudentService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class StudentController {
@@ -18,5 +21,18 @@ public class StudentController {
     public String listStudents(Model model){
         model.addAttribute("students",studentService.getAllStudents()); //model is used to pass data to view
         return "students";
+    }
+
+    @GetMapping("/students/new")
+    public String createStudentForm(Model model){
+        Student student = new Student();
+        model.addAttribute("student",student);
+        return "create_student";
+    }
+
+    @PostMapping("/students")
+    public String saveStudent(@ModelAttribute("student") Student student){
+        studentService.saveStudent(student);
+        return "redirect:/students";
     }
 }
